ecc505f25b06e55334e13625764e6c58b7acf59e
reynoyas <yasilizbeth@gmail.com>
2023-04-18 13:44:18 -0700

    Installed matter.js, used library to simulate 2d physics

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Matter from 'matter-js';
+
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Bodies = Matter.Bodies,
+    Composite = Matter.Composite;
+
+const iEngine = Engine.create();
+const iRunner = Runner.create();
+
+const iRender = Render.create({
+  element: document.body,
+  engine: iEngine,
+  options: {
+    width: 800,
+    height: 400,
+    wireframes: false,
+    background: "white"
+  }
+});
+
+const boxA = Bodies.rectangle(400, 200, 80, 80);
+const ballA = Bodies.circle(380, 100, 40, 10);
+const ballB = Bodies.circle(460, 10, 40, 10);
+const ground = Bodies.rectangle(400, 380, 810, 60, { isStatic: true });
+
+Composite.add(iEngine.world, [boxA, ballA, ballB, ground]);
+Render.run(iRender);
+Runner.run(iRunner, iEngine);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
